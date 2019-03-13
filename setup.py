f1ef862aea28ce35c128d395b4bff024ef29f468
Keenan Graham <keenangraham@stanford.edu>
2019-03-12 22:08:41 -0700

    Update dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setup(
         "pandas >= 0.23.4",
         "theano >= 1.0.1",
         "keras >= 2.0.8",
-        "tqdm >= 4.24.0"
+        "tqdm >= 4.24.0",
+        "tensorflow >= 1.10.0",
+        "numba >= 0.42.1",
     ]
 )
